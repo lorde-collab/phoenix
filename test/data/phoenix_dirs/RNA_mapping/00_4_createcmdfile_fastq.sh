02817bd2dff6e34c4fe8cb01654f89ef44adc095
J. Robert Michael <jrobert.michael@stjude.org>
2019-10-09 14:37:24 -0500

    Added RNA mapping from actual run

--- test/data/phoenix_dirs/RNA_mapping/00_4_createcmdfile_fastq.sh
+++ test/data/phoenix_dirs/RNA_mapping/00_4_createcmdfile_fastq.sh
@@ -2,11 +2,11 @@
 # Convert ubam to fastq
 
 # Run from run dir for simplicity
-#cd /research/rgs01/scratch/tartan_prod/tIUDDrvkJu/workspace
+cd /research/rgs01/scratch/tartan_prod/tIUDDrvkJu/workspace
 
 # Make commands script:
 for sample in SJMMINF063855_D1 SJMMINF063856_D1 SJMMINF063857_D1 SJMMINF063858_D1 SJMMINF063859_D1 
 do
   # Write commands
-  echo "echo /research/rgs01/resgen/prod/tartan/runs/RNA_mapping/ZzV6R6wi/intmd/$sample/ubam /research/rgs01/resgen/prod/tartan/runs/RNA_mapping/ZzV6R6wi/intmd/$sample/fastq && sleep 4"
+  make_script_ubam_to_fastq.sh /research/rgs01/resgen/prod/tartan/runs/RNA_mapping/ZzV6R6wi/intmd/$sample/ubam /research/rgs01/resgen/prod/tartan/runs/RNA_mapping/ZzV6R6wi/intmd/$sample/fastq
 done > /research/rgs01/scratch/tartan_prod/tIUDDrvkJu/workspace/cmds-00.sh
