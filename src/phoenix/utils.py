e36a19442903b5854e2af6e32194f3bc2b87d59b
J. Robert Michael <jrobert.michael@stjude.org>
2019-12-07 12:46:37 -0600

    Added updated license

--- src/phoenix/utils.py
+++ src/phoenix/utils.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
-""" Utilities for all of phoenix """
+"""
+Utilities for all of phoenix
+
++--------------------------------------------------------------------------+
+| Copyright 2019 St. Jude Children's Research Hospital                     |
+|                                                                          |
+| Licensed under a modified version of the Apache License, Version 2.0     |
+| (the "License") for academic research use only; you may not use this     |
+| file except in compliance with the License. To inquire about commercial  |
+| use, please contact the St. Jude Office of Technology Licensing at       |
+| scott.elmer@stjude.org.                                                  |
+|                                                                          |
+| Unless required by applicable law or agreed to in writing, software      |
+| distributed under the License is distributed on an "AS IS" BASIS,        |
+| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+| See the License for the specific language governing permissions and      |
+| limitations under the License.                                           |
++--------------------------------------------------------------------------+
+"""
 
 import argparse
 import glob
@@ -12,7 +30,7 @@ import datetime
 
 from phoenix import __version__
 from phoenix.constants import (
-    SUB_UPDATE_INTERVAL, SUBSTEP_TYPES, MEMLIM_DEFAULT, SPLIT_CUTOFF)
+    SUB_UPDATE_INTERVAL, SUBSTEP_TYPES, MEMLIM_DEFAULT)
 
 def now():
     """ Simple method to get current datetime """
